91071d33f4deb1309f3025606d1496fd5cf8252a
Jose <mailtitto@gmail.com>
2017-03-23 00:32:01 +0530

    Recycler view integration.

--- app/src/main/java/me/tittojose/dagger2tutorial/screens/MainActivity.java
+++ app/src/main/java/me/tittojose/dagger2tutorial/screens/MainActivity.java
@@ -2,10 +2,18 @@ package me.tittojose.dagger2tutorial.screens;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import me.tittojose.dagger2tutorial.Dagger2TutorialApp;
 import me.tittojose.dagger2tutorial.R;
+import me.tittojose.dagger2tutorial.model.Movie;
 import me.tittojose.dagger2tutorial.model.MoviesList;
 import me.tittojose.dagger2tutorial.network.MovieAPIService;
 import retrofit2.Call;
@@ -17,10 +25,16 @@ public class MainActivity extends AppCompatActivity {
     MovieAPIService movieAPIService;
     Call<MoviesList> movieListRequest;
 
+    RecyclerView rvMovies;
+    private MoviesAdapter mAdapter;
+    private List<Movie> mMovieList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeUi();
 
         movieAPIService = Dagger2TutorialApp.get(this).getMovieAPIService();
 
@@ -30,8 +44,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<MoviesList> call, Response<MoviesList> response) {
                 MoviesList moviesList = response.body();
-                Toast.makeText(MainActivity.this, moviesList.results.size()+"", Toast.LENGTH_SHORT).show();
-
+                Toast.makeText(MainActivity.this, moviesList.results.size() + "", Toast.LENGTH_SHORT).show();
+                mMovieList.addAll(moviesList.results);
+                mAdapter = new MoviesAdapter(mMovieList);
+                rvMovies.setAdapter(mAdapter);
             }
 
             @Override
@@ -39,7 +55,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
 
+    private void initializeUi() {
+        rvMovies = (RecyclerView) findViewById(R.id.rvMovies);
+        mAdapter = new MoviesAdapter(mMovieList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        rvMovies.setLayoutManager(mLayoutManager);
+        rvMovies.setItemAnimator(new DefaultItemAnimator());
+        rvMovies.setAdapter(mAdapter);
 
     }
 
---
+++ app/src/main/java/me/tittojose/dagger2tutorial/screens/MoviesAdapter.java
@@ -0,0 +1,66 @@
+package me.tittojose.dagger2tutorial.screens;
+
+
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import me.tittojose.dagger2tutorial.R;
+import me.tittojose.dagger2tutorial.model.Movie;
+
+class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private List<Movie> moviesList;
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvItemMovieTitle;
+        ImageView imgViewItemMoviePoster;
+
+        MyViewHolder(View view) {
+            super(view);
+            tvItemMovieTitle = (TextView) view.findViewById(R.id.tvItemMovieTitle);
+            imgViewItemMoviePoster = (ImageView) view.findViewById(R.id.imgViewItemMoviePoster);
+
+        }
+    }
+
+
+    MoviesAdapter(List<Movie> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_movie, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Movie movie = moviesList.get(position);
+
+        String url = "http://image.tmdb.org/t/p/w185/" + movie.posterPath;
+        holder.tvItemMovieTitle.setText(movie.title);
+
+        Glide.with(holder.imgViewItemMoviePoster.getContext())
+                .load(url)
+                .centerCrop()
+                .into(holder.imgViewItemMoviePoster);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
+}
